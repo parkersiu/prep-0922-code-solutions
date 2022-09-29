6a04d7eb74b8bde374434355deae05e94192dc9f
Parker Siu <parker.siu@gmail.com>
2022-09-29 03:17:14 +0000

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 9; i++) {
+  console.log('first statement:', i);
+}
+
+for (let x = 0; x < 18; 1 + x++) {
+  console.log('second statement:', x);
+}
+
+for (let z = 100; z > 0; z--) {
+  console.log('Time till explosion ', z);
+}
